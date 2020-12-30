205ed76fa8acb7ae30def4f6dd4504e5b7379881
Coty Tiff <coty.tiff@gmail.com>
2020-12-30 19:32:03 -0300

    arreglando carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Logo
 $( document ).ready(function() {
     $("#logoB").hide();
 });
@@ -10,3 +11,12 @@ $("#logoB").mouseleave(function () {
     $("#logoB").toggle(800);
     $("#logoA").toggle(800);
 });
+// cads
+$("#card-1").click(function(){
+    $("#card-2").toggle(800);
+    $("#card-3").toggle(800);
+    $("#card-4").toggle(800);
+    $("#card-5").toggle(800);
+    $("#card-6").toggle(800);
+    $("#card-1").show(800);
+  });
